e1c498e927a75646c716775ebbb4ceb6372a924a
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2023-01-04 01:44:34 -0600

    Add mobile styling to aux pages.

--- portfolio/src/pages/ConstructionPage.tsx
+++ portfolio/src/pages/ConstructionPage.tsx
@@ -1,9 +1,9 @@
 function ConstructionPage() {
     
     return (
-        <div className=" flex flex-col items-center pt-48">
-            <h1 className="font-bold text-9xl">HI!</h1>
-            <h2 className=" text-5xl">SORRY, I AM STILL UNDER CONSTRUCTION</h2>
+        <div className="flex flex-col text-center items-center md:pt-48">
+            <h1 className="font-bold text-7xl md:text-9xl">HI!</h1>
+            <h2 className="px-10 text-5xl">SORRY, I AM STILL UNDER CONSTRUCTION</h2>
         </div>
     )
 }
