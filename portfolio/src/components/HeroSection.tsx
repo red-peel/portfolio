930f07da4ab7532a275e96e283e8dbf739f851a9
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2023-01-04 01:18:21 -0600

    Update HeroSection.tsx

--- portfolio/src/components/HeroSection.tsx
+++ portfolio/src/components/HeroSection.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function HeroSection() {
     return (
-        <div className="mx-12 p-12">
-            <div className="max-w-4xl mx-auto text-white">
-                <div className='text-slate-900'>
+        <div className='bg-gradient-to-br from-orange-500 p-10 md:p-40'>
+            <div className="max-w-4xl mx-auto ">
+                <div className='text-slate-900 text-left '>
                     <p className='text-3xl'> Elias Gonzalez</p>
                     <p className='text-xl font-thin'> Fullstack Developer </p>
-                    <p className="text-5xl leading-tight">Hello! I am Elias Gonzalez a Fullstack <br/>Java-React developer based out of San Antonio, Texas!</p>
+                    <p className="md:text-5xl leading-tight">Hello! I am Elias Gonzalez a Fullstack <br/>Java-React developer based out of San Antonio, Texas!</p>
                 </div>
             </div>
         </div>
