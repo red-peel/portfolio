eb3e389220b7067f48a1e6d3e1cec7cb46d97f1e
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2023-01-05 00:30:15 -0600

    Update Navbar.tsx

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -8,7 +8,7 @@ function Navbar(){
     <nav className="flex-none m-10 flex flex-col md:flex-row place-content-between pr-2">
         <div className='md:inline-flex md:self-center'>
             <img className='max-h-10 max-w-10 ' src="logo192.png" alt="" />
-            <h1 id="title" className="inline-block pr-2 md:pl-3 text-4xl font-mono transition ease-in-out hover:opacity-60 duration-300 "> <a href="https://github.com/red-peel"> &#123;ELIAS_GONZALEZ&#125; </a> </h1>
+            <h1 id="title" className="inline-block pr-2 md:pl-3 text-4xl font-mono transition ease-in-out hover:opacity-60 duration-300 "> <a className="" href="https://github.com/red-peel"> &#123;ELIAS_GONZALEZ&#125;<p className="inline-block opacity-0 md:opacity-100 md:animate-ping">|</p> </a> </h1>
         </div>
         <menu className="flex flex-col md:flex-row sm:flex-col">
             {navbarMenuItems.map(([title, link]) => {
