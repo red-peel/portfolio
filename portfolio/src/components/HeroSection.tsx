9da5cf0173e46b6b9f6ca58a9f7aaecfe8cbe953
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2023-01-04 19:47:52 -0600

    Add custom media bgs

--- portfolio/src/components/HeroSection.tsx
+++ portfolio/src/components/HeroSection.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function HeroSection() {
     return (
-        <div className='bg-gradient-to-br from-orange-500 p-10 md:p-40'>
+        <div className='bg-hero-pattern p-10 md:p-40'>
             <div className="max-w-4xl mx-auto ">
-                <div className='text-slate-900 text-left '>
+                <div className='bg-black bg-opacity-90 p-10 text-white text-left rounded'>
                     <p className='text-3xl'> Elias Gonzalez</p>
                     <p className='text-xl font-thin'> Fullstack Developer </p>
                     <p className="md:text-5xl leading-tight">Hello! I am Elias Gonzalez a Fullstack <br/>Java-React developer based out of San Antonio, Texas!</p>
