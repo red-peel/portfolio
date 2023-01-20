463acb38007874e6a6d2c2cca9aa850853d9e8c5
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2023-01-19 20:05:54 -0600

    Update ContactCard.tsx

--- portfolio/src/components/ContactCard.tsx
+++ portfolio/src/components/ContactCard.tsx
@@ -2,9 +2,9 @@ function ContactCard() {
     return (
         <section className="bg-gray-50 opacity-90 p-5 w-full gap-3 flex flex-col text-xl text-left md:rounded-md text-black md:opacity-100 md:w-max lg:p-20 md:p-10 md:mb-8 md:shadow-lg md:text-4xl">
             <img className="w-8"src="800blk.png" alt="" />
-            <p className="">Elias Gonzalez</p>
-            <p className="">210-373-9322</p>
-            <p className="">elias@eliasgonzalez.dev</p>
+            <address className="">Elias Gonzalez</address>
+            <address className="">210-373-9322</address>
+            <address className="">elias@eliasgonzalez.dev</address>
         </section>
     )
 }
